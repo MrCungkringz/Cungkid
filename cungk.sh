23905be6eaa914f6d1f216c433fcb93bd178d513
MrCungkringz <45120964+MrCungkringz@users.noreply.github.com>
2018-11-22 22:06:08 +0700

    Add files via upload

---
+++ cungk.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+clear
+echo "Mr.Cungkringz./" | lolcat;
+sleep 0.9;
+clear
+echo 'Mr.Cungkringz..-' | lolcat;
+sleep 0.9;
+clear
+echo 'Mr.Cungkringz...\' | lolcat;
+sleep 0.9;
+clear
+echo 'Mr.Cungkringz....|' | lolcat;
+sleep 0.9;
+clear
+echo 'Mr.Cungkringz...../' | lolcat;
+sleep 0.9;
+clear
+echo 'Mr.Cungkringz......-' | lolcat;
+sleep 0.9;
+clear
+echo 'Mr.Cungkringz.......\' | lolcat;
+sleep 0.9;
+clear
+echo 'Mr.Cungkringz........|' | lolcat;
+sleep 0.9;
+clear
+echo 'Mr.Cungkringz........./' | lolcat;
+sleep 1.2;
+clear
+echo "Ketik kontol untuk lanjutkan" |lolcat
+read -p "MrCungkringz=" kontol
+clear
+
+if [ $kontol = kontol ]
+then
+echo "=================================================="
+echo "___________ !!! SCRIPT KIDDIE !!!________________ "
+echo "      ||Author  ||Mr.Cungkring             ||"
+echo "      ||Contact ||08996226529              ||"
+echo "      ||Grup    ||Grup Termux Indonesia    ||"
+echo "      ||Email   ||ramdanikuncoro@gmail.com ||"
+echo "=================================================="
+date
+echo "»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»"
+echo "•••••••••••••••••••••••"
+echo " (1)~~ DarkFly-Tool~~"
+echo "•••••••••••••••••••••••"
+echo " (2)~~ Mr.Cakil ~~"
+echo "•••••••••••••••••••••••"
+echo " (3)~~ BAJINGANv6 ~~"
+echo "•••••••••••••••••••••••"
+echo " (4)~~Link Bokep ~~"
+echo "•••••••••••••••••••••••"
+read -p "Mr.Cungkringz=" bro
+
+if [ $bro = 1 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+python2 install.py
+DarkFly
+fi
+
+if [ $bro = 2 ]
+then
+clear
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/mrcakil/Mrcakil.git
+cd Mrcakil
+chmod +x tools
+./tools
+
+fi
+
+if [ $bro = 3 ]
+then
+clear
+apt install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+BAJINGAN
+Gans
+
+fi
+
+if [ $bro = 4 ]
+then
+echo ".,.,.,.,.,.,,.,TOBAT KONTOL NONTON BOKEP MULU
+.,.,..,..,,"
+
+fi
+
+fi
